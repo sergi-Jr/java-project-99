1e1099c8dbbba825a1d0e959e56df4e5f9fed539
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-18 04:05:04 +0400

    [skip ci] add user dto

---
+++ app/src/main/java/hexlet/code/app/user/UserDTO.java
@@ -0,0 +1,20 @@
+package hexlet.code.app.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class UserDTO {
+    private long id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate createdAt;
+}

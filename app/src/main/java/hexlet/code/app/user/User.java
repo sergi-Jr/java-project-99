3cb1411455a2d53caab4b91945097c664698151c
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-19 03:34:55 +0400

    [skip ci] fix non-requierd fields. remove @notNull constraint

--- app/src/main/java/hexlet/code/app/user/User.java
+++ app/src/main/java/hexlet/code/app/user/User.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -34,10 +33,8 @@ public class User implements BaseEntity, UserDetails {
     @GeneratedValue(strategy = IDENTITY)
     private long id;
 
-    @NotNull
     private String firstName;
 
-    @NotNull
     private String lastName;
 
     @Email

268c4215b927311f4a355239abca5b20c58366b8
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-18 22:53:32 +0400

    [skip ci] add user details service implementation

---
+++ app/src/main/java/hexlet/code/app/user/SimpleUserDetailsService.java
@@ -0,0 +1,19 @@
+package hexlet.code.app.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found by: " + email));
+    }
+}

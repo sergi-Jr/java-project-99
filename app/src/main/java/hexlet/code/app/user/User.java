996315fc16b3324057ee8b7e5944f8b051abc009
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-18 22:50:54 +0400

    [skip ci] add entity auditing

--- app/src/main/java/hexlet/code/app/user/User.java
+++ app/src/main/java/hexlet/code/app/user/User.java
@@ -2,6 +2,7 @@ package hexlet.code.app.user;
 
 import hexlet.code.app.model.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -13,16 +14,22 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "users")
 @Getter
 @Setter
-public class User implements BaseEntity {
+public class User implements BaseEntity, UserDetails {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private long id;
@@ -46,4 +53,14 @@ public class User implements BaseEntity {
 
     @LastModifiedDate
     private LocalDate updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "ROLE_ADMIN");
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }

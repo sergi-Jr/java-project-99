c1dffe562725f9c5649ce3553fadce1e5ca9a9eb
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-18 04:04:22 +0400

    [skip ci] add user dto for model updating

---
+++ app/src/main/java/hexlet/code/app/user/UserUpdateDTO.java
@@ -0,0 +1,24 @@
+package hexlet.code.app.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+@Getter
+@Setter
+public class UserUpdateDTO {
+    @Email
+    @NotBlank
+    private JsonNullable<String> email;
+
+    private JsonNullable<String> firstName;
+
+    private JsonNullable<String> lastName;
+
+    @NotBlank
+    @Size(min = 3)
+    private JsonNullable<String> password;
+}

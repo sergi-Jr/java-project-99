537bd6dce8b97208526ee7139412349f32080cc3
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-18 04:02:18 +0400

    [skip ci] add spring-security

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    //implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     //implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("net.datafaker:datafaker:2.2.2")
     implementation("org.instancio:instancio-junit:4.8.0")

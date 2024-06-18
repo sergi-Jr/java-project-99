dc745f85b9b6fc8d97fe775d5e8c0b3437da2666
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-18 04:06:11 +0400

    [skip ci] set user entity validation annotations

--- app/src/main/java/hexlet/code/app/user/User.java
+++ app/src/main/java/hexlet/code/app/user/User.java
@@ -1,10 +1,14 @@
 package hexlet.code.app.user;
 
+import hexlet.code.app.model.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
@@ -18,18 +22,23 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Table(name = "users")
 @Getter
 @Setter
-public class User {
+public class User implements BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private long id;
 
+    @NotNull
     private String firstName;
 
+    @NotNull
     private String lastName;
 
     @Email
+    @NotBlank
     private String email;
 
+    @NotBlank
+    @Size(min = 3)
     private String password;
 
     @CreatedDate

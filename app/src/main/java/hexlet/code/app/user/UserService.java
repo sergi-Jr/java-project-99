5b1e912b7e932dc9ac0fea65f68368b5eaceafde
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-19 03:49:04 +0400

    [skip ci] add transactional annotation to service

--- app/src/main/java/hexlet/code/app/user/UserService.java
+++ app/src/main/java/hexlet/code/app/user/UserService.java
@@ -3,10 +3,12 @@ package hexlet.code.app.user;
 import hexlet.code.app.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class UserService {
     @Autowired
     private UserRepository repository;
@@ -25,12 +27,14 @@ public class UserService {
         return mapper.map(user);
     }
 
+    @Transactional
     public UserDTO add(UserCreateDTO data) {
         User user = mapper.map(data);
         repository.save(user);
         return mapper.map(user);
     }
 
+    @Transactional
     public UserDTO update(Long id, UserUpdateDTO data) {
         User user = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Not found"));
         mapper.update(data, user);
@@ -38,6 +42,7 @@ public class UserService {
         return mapper.map(user);
     }
 
+    @Transactional
     public void delete(Long id) {
         repository.deleteById(id);
     }

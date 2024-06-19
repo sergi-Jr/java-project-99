6ed2ede67386e09a99752ae655a3e544b482f886
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-19 05:14:53 +0400

    commented file

--- app/src/test/java/hexlet/code/app/AppApplicationTests.java
+++ app/src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -1,13 +1,13 @@
-package hexlet.code.app;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package hexlet.code.app;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}

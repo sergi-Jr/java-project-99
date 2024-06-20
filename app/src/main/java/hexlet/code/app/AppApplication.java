f6a620038d8dc16738d5d92835ede368f7b6b22a
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-20 07:20:07 +0400

    add DataFaker bean

--- app/src/main/java/hexlet/code/app/AppApplication.java
+++ app/src/main/java/hexlet/code/app/AppApplication.java
@@ -1,8 +1,12 @@
 package hexlet.code.app;
 
+import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,5 +14,10 @@ public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @Bean
+    public static Faker getFaker() {
+        return new Faker(new Locale("en", "US"));
     }
 }

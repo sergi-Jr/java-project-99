6e15eddce25a614a8d467f7301325ae0c6fc3e09
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-20 07:26:55 +0400

    add pre authorize conditions

--- app/src/main/java/hexlet/code/app/user/api/UserController.java
+++ app/src/main/java/hexlet/code/app/user/api/UserController.java
@@ -8,6 +8,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@EnableMethodSecurity
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -28,30 +31,37 @@ public class UserController {
 
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("@RoleService.hasAnyRoleByUserId(@UserRole.user)")
     public List<UserDTO> index() {
         return userService.getAll();
     }
 
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("@RoleService.hasAnyRoleByUserId(@UserRole.user)")
     public UserDTO show(@PathVariable Long id) {
         return userService.getOneById(id);
     }
 
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("@RoleService.hasAnyRoleByUserId(@UserRole.admin)")
     public UserDTO create(@Valid @RequestBody UserCreateDTO data) {
         return userService.add(data);
     }
 
     @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("@RoleService.isTrueUserOwnerByUserId(#id) AND"
+            + "@RoleService.hasAnyRoleByUserId(@UserRole.owner)")
     public UserDTO update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO data) {
         return userService.update(id, data);
     }
 
     @DeleteMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("@RoleService.isTrueUserOwnerByUserId(#id) AND"
+            + "@RoleService.hasAnyRoleByUserId(@UserRole.owner)")
     public void delete(@PathVariable Long id) {
         userService.delete(id);
     }

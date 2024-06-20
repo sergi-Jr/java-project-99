fd97837f0b8ad447769b881150c8111b5e69c3e7
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-20 07:32:12 +0400

    add simple task entity

---
+++ app/src/main/java/hexlet/code/app/task/Task.java
@@ -0,0 +1,16 @@
+package hexlet.code.app.task;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+}

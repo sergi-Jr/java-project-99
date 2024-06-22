58b7f528982477f84a1f8e56a0393be5d86679ed
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-22 11:07:46 +0400

    add exception handler advice

--- app/src/main/java/hexlet/code/app/handler/GlobalExceptionHandler.java
+++ app/src/main/java/hexlet/code/app/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package hexlet.code.app.handler;
 
 import hexlet.code.app.exception.ResourceNotFoundException;
+import hexlet.code.app.exception.UnableDeletionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,5 +13,10 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UnableDeletionException.class)
+    public ResponseEntity<String> handleUnableDeletion(UnableDeletionException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(exception.getMessage());
     }
 }

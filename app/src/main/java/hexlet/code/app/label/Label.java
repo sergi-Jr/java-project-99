0128dbd9eb81b54ec31d0063a3bc62ce7600e4b6
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-25 08:38:20 +0400

    remove task relation

--- app/src/main/java/hexlet/code/app/label/Label.java
+++ app/src/main/java/hexlet/code/app/label/Label.java
@@ -2,15 +2,12 @@ package hexlet.code.app.label;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import hexlet.code.app.serializer.LabelSerializer;
-import hexlet.code.app.task.model.Task;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,8 +16,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
@@ -37,9 +32,6 @@ public class Label {
     @Column(unique = true, columnDefinition = "TEXT")
     @ToString.Include
     private String name;
-
-    @ManyToMany(mappedBy = "labels", fetch = FetchType.LAZY)
-    private List<Task> tasks = new ArrayList<>();
 
     @CreatedDate
     private LocalDate createdAt;

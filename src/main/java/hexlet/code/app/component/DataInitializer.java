f9f3bfaccceb23548cacca50d541f581064e27e8
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-06-30 17:56:49 +0400

    [skip ci] remove unused import

--- src/main/java/hexlet/code/app/component/DataInitializer.java
+++ src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -12,7 +12,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
